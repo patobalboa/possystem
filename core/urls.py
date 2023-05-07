62f83829da146524db18dc79a21440882a609553
Pato Balboa <pbalboa08@gmail.com>
2023-05-07 03:06:18 -0400

    Falta arreglar  la importacion de lista de categor

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
 #from core.views.categories.views import category_list
 from . import views
-from core.views.categories.views import category_list
+#from core.views.categories.views import category_list
 
 app_name = 'core'
 
 app_name = 'core'
 
 urlpatterns = [
-<<<<<<< HEAD
-    #path('', views.index, name='index'),
-    #path('categories/list/', category_list, name='category_list')
-=======
     path('', views.index, name='index'),
-    path('categories/list/', category_list, name='category_list'),
->>>>>>> dev
+    #path('categories/list/', category_list, name='category_list'),
 ]
