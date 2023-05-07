6c3b121ce459992d9e3da839efd9b8f151b07ccc
Pato Balboa <pbalboa08@gmail.com>
2023-05-07 02:57:38 -0400

    cambios realizados

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Perfil)
+admin.site.register(Empleado)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
+admin.site.register(Proveedor)
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from core.views.categories.views import category_list
+
+app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('categories/list/', category_list, name='category_list'),
 ]
