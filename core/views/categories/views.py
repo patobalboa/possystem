9d02fdc47a1b1730b7ee52d9a8323adaa2853a80
Pato Balboa <pbalboa08@gmail.com>
2023-05-08 02:10:44 -0400

    Agregar clases

--- core/views/categories/views.py
+++ core/views/categories/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from core import db
-
+from django.views.generic import ListView
+from core.models import Categoria
 
 def category_list(request):
-    data = db.categories.findAll()
+    data ={
+        'title': 'Listado de Categorias',
+        'categories': Categoria.objects.all()
+    }
     return render(request, 'categories/list.html', data)
+
+class CategoriaListView(ListView):
+    model = Categoria
+    template_name = 'categories/list.html'
+    context_object_name = 'categories'
+    #paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorias'
+        return context
     
